b322ad9a2d041822ab0499d950b1edf8119fba04
andrews1022 <andrewshearerdev@gmail.com>
2022-02-07 13:42:47 -0800

    cleaned up boilerplate code, added few hooks

---
+++ src/hooks/useScroll/useScroll.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+
+import { AnimationControls, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const useScroll = (): [
+	element: (node?: Element | null | undefined) => void,
+	controls: AnimationControls
+] => {
+	const controls = useAnimation();
+	const [element, view] = useInView({ threshold: 0.2, triggerOnce: true });
+
+	if (view) {
+		controls.start('show');
+	} else {
+		controls.start('hidden');
+	}
+
+	return [element, controls];
+};
+
+export default useScroll;
